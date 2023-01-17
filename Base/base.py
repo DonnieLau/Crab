ee8b7e397d07a5ee4799ff91afdbdb39b5030566
doonie <doonie@163.com>
2023-01-17 16:36:06 +0800

    update

--- Base/base.py
+++ Base/base.py
@@ -2,6 +2,8 @@ import os
 import time
 
 from loguru import logger
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 png_path = ""
 
@@ -34,15 +36,62 @@ class BasePage:
             raise
 
     def get_element(self, by, value, doc=''):
+        """
+        定位元素
+        :param by:定位方法
+        :param value:定位表达式
+        :param doc:模块信息
+        :return:返回定位元素，如果失败则抛出异常
+        """
         try:
-            ele = self.driver.find_element(by, value)
-            logger.info(f"模块:{doc}，获取元素成功，定位方法{by}，定位表达式{value}")
-            return ele
+            element = self.driver.find_element(by, value)
+            logger.info(f"模块：{doc}，获取元素成功，定位方法{by}，定位表达式{value}")
+            return element
         except:
-            logger.error(f"模块:{doc}，获取元素失败，定位方法{by}，定位表达式{value}")
+            logger.error(f"模块：{doc}，获取元素失败，定位方法{by}，定位表达式{value}")
             self.save_screenshot(doc)
             raise
 
+    def wait_element_presence(self, locator, timeout=10, doc=''):
+        """
+        等待获取元素
+        :param locator:元素定位方式和定位表达式
+        :param timeout:等待时间，默认10s
+        :param doc:模块信息
+        :return:返回定位元素，如果失败则抛出异常
+        """
+        try:
+            element = WebDriverWait(self.driver, timeout).until(expected_conditions.presence_of_element_located(locator))
+            logger.info(f"模块：{doc}等待元素成功，定位方式{locator}")
+            return element
+        except:
+            logger.error(f"模块：{doc}等待元素失败，定位方式{locator}")
+            self.save_screenshot(doc)
+            raise
+
+    def url_matches(self, url, timeout=10, doc="url路径匹配"):
+        """
+        等待路径匹配
+        :param url: 期望的url路径
+        :param timeout: 等待时间，默认10s
+        :param doc: 模块信息
+        :return: 无返回
+        """
+        try:
+            WebDriverWait(self.driver, timeout).until(expected_conditions.url_matches(url))
+            logger.info(f"{doc}:页面路径:{self.driver.current_url}, 匹配路径:{url}")
+        except:
+            logger.error(f"{doc}:页面路径:{self.driver.current_url}, 匹配路径:{url}")
+            self.save_screenshot(doc)
+            raise
+
+    def quit(self):
+        """
+        退出浏览器
+        :return:无返回
+        """
+        self.driver.quit()
+        logger.info(f"退出浏览器成功")
 
 if __name__ == '__main__':
     from selenium import webdriver
